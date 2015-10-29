af5d3c5f59a0c3b72d17c57b7d184b8043bf1613
dadiletta <dan.adiletta@gmail.com>
2015-10-29 09:00:11 -0400

    Added stop and fwd

--- Pigo-adiletta.py
+++ Pigo-adiletta.py
@@ -1,10 +1,23 @@
-#from gopigo import *
+from gopigo import *
+import time
 
 __author__ = 'adilettad'
 
 class Pigo:
 
+    isMoving = False
+
     def __init__(self):
-        print "I'm such a pigo. beep beep."
+        print "I'm a little robot car! Beep beep."
+
+    def stop(self):
+        self.isMoving = False
+        while stop() != 1:
+            time.sleep(.1)
+            print "Eek! I'm having trouble stopping."
+            stop()
+
+
 
 tina = Pigo()
+tina.stop()
---
+++ PigoRecreate.py
@@ -0,0 +1,29 @@
+from gopigo import *
+import time
+
+class Pigo:
+
+    isMoving = False
+    servoPos = 90
+
+    def __init__(self):
+        print "I'm a little robot car. beep beep."
+
+    #named after the GoPiGo version of stop()
+    def stop(self):
+        self.isMoving = False
+        while stop() != 1:
+            time.sleep(.1)
+            print "Whoops, sorry boss. Can't stop."
+
+    def fwd(self):
+        self.isMoving = True
+        while fwd() != 1:
+            time.sleep(.1)
+            print "Can't do the vroom vroom."
+
+tina = Pigo()
+tina.fwd()
+time.sleep(2)
+tina.stop()
+
