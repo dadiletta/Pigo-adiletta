42d1ae2e5ef75212d256dce141f296238f44e6db
dadiletta <dan.adiletta@gmail.com>
2015-11-09 11:43:34 -0500

    added methods and an allstop file

--- Pigo.py
+++ Pigo.py
@@ -1,5 +1,7 @@
 from gopigo import *
 import time
+
+#Global variable on how close an object is allowed to get
 STOP_DIST = 50
 
 class Pigo:
@@ -13,7 +15,7 @@ class Pigo:
 
     def __init__(self):
         print "I'm a little robot car. beep beep."
-        self.status['dist'] = us_dist(15)
+        self.checkDist()
 
     def stop(self):
         self.status["ismoving"] = False
@@ -27,6 +29,12 @@ class Pigo:
         for x in range(3):
             fwd()
 
+    def bwd(self):
+        self.status["ismoving"] = True
+        print "Back, back it up!"
+        for x in range(3):
+            bwd()
+
     def keepGoing(self):
         if self.status['dist'] < STOP_DIST:
             return False
@@ -36,6 +44,9 @@ class Pigo:
     def checkDist(self):
         self.status['dist'] = us_dist(15)
         print "I see something " + str(self.status['dist']) + "mm away."
+        if not self.keepGoing():
+            print "EMERGENCY STOP FROM THE CHECK DISTANCE METHOD!"
+            self.stop()
 
     #######
     #######  ADVANCED METHODS
@@ -43,20 +54,19 @@ class Pigo:
 
     def dance(self):
         print "I just want to DANCE!"
-        self.spin()
-        self.shuffle()
-        self.shakeServo()
-        self.rturn()
-        self.lturn()
-        self.blink()
-        
+        if self.keepGoing():
+            self.circleRight()
+            self.circleLeft()
+            self.shuffle()
+            self.servoShake()
+            self.blink()
+
+
 #######
 #######  MAIN APP STARTS HERE
 #######
 tina = Pigo()
-
 while tina.keepGoing():
-    tina.checkDist()
     tina.fwd()
     time.sleep(2)
     tina.stop()
